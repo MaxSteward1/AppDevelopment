da620461fe273c3bcb9d1facb069017193bd2e28
Max Steward <max.steward@o2.com>
2017-11-21 10:56:10 +0000

    Hides the back button on the results view, added function to the 'Done' button. This is the completed guided project.

--- PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -18,6 +18,7 @@ class ResultsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
         // Do any additional setup after loading the view.
     }
     
