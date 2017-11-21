db413dad5576cde47932ee62e0f77712cd7ff9f2
Max Steward <max.steward@o2.com>
2017-11-21 10:49:46 +0000

    Added the results screen and completed the results calculation with visual confirmation to the user

--- PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    var responses: [Answer]!
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        calculatePersonalityResult()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func calculatePersonalityResult() {
+        var frequencyOfAnswer: [AnimalType: Int] = [:]
+        let responseTypes = responses.map {$0.type}
+        
+        for response in responseTypes {
+            frequencyOfAnswer[response] = (frequencyOfAnswer[response] ?? 0) + 1
+        }
+        
+        let frequencyAnsweredSorted = frequencyOfAnswer.sorted(by: {(pair1, pair2) -> Bool in return pair1.value > pair2.value})
+        
+        let mostCommonAnswer = frequencyAnsweredSorted.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
     }
     
 
